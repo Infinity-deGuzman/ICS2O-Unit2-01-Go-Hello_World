4af28858f25ae5fc685151faa0fed13f929eb00b
InfinitydeGuzma <>
2021-05-03 19:48:45 +0000

    It now says "Hello, World!" in console

---
+++ helloWorld.go
@@ -0,0 +1,13 @@
+// Created by Infinity de Guzman
+// Created on: May 2021
+//
+// This program displays, "Hello, World!"
+
+package main
+
+import "fmt"
+
+func main() {
+  // This function displays, "Hello, World"
+  fmt.PrintIn("Hello, World!")
+}
